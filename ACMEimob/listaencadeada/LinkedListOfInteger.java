1a5f67635decfdb8787bb6a0ecde42818973c1b8
Douglas Bitencourte Vivian <82385510+dougvivian@users.noreply.github.com>
2026-04-07 01:18:06 +0000

    aula 05

---
+++ ACMEimob/listaencadeada/LinkedListOfInteger.java
@@ -0,0 +1,160 @@
+public class LinkedListOfInteger {
+
+    // Classe interna Node
+    private class Node {
+        public Integer element;
+        public Node next;
+
+        public Node(Integer element) {
+            this.element = element;
+            next = null;
+        }
+
+        public Node(Integer element, Node next) {
+            this.element = element;
+            this.next = next;
+        }
+    }
+
+    // Referência para o primeiro elemento da lista encadeada.
+    private Node head;
+    // Referência para o último elemento da lista encadeada.
+    private Node tail;
+    // Contador para a quantidade de elementos que a lista contem.
+    private int count;
+
+    /**
+     * Construtor da lista.
+     */
+    public LinkedListOfInteger() {
+        head = null;
+        tail = null;
+        count = 0;
+    }
+
+    /**
+     * Esvazia a lista
+     */
+    public void clear() {
+        head = null;
+        tail = null;
+        count = 0;
+    }
+
+    /**
+     * Adiciona um elemento ao final da lista.
+     *
+     * @param element elemento a ser adicionado ao final da lista
+     */
+    public void add(Integer element) {
+        Node n = new Node(element);
+        if (head == null) {
+            head = n;
+        } else {
+            tail.next = n;
+        }
+        tail = n;
+        count++;
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+
+        Node aux = head;
+
+        while (aux != null) {
+            s.append(aux.element.toString());
+            s.append("\n");
+            aux = aux.next;
+        }
+
+        return s.toString();
+    }
+
+    /////////////////////////////////////////////////// / EXERCICIOS - VEJA SLIDES
+    /// E ENUNACIADO
+    ///////////////////////////////////////////////////
+
+    public boolean isEmpty() {
+        if (head == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public Integer get(int index) {
+        Node atual = head;
+        int contador = 0;
+
+        while (atual != null) {
+            if (contador == index) {
+                return atual.element;
+            }
+            atual = atual.next;
+            contador++;
+        }
+        return null;
+    }
+    // tentar fazer
+    public void switchElement(Integer element) {
+        Node atual = head;
+        int contador = 0;
+
+        while (atual != null) {
+            if (element == atual.element) {
+                atual.element = element;
+            }
+            atual = atual.next;
+            contador++;
+        }
+
+    }
+    // 1 - implemente o método isEmpty
+    /*
+     * Retorna true se a lista nao contem elementos.
+     *
+     * @return true se a lista nao contem elementos
+     */
+    // assinatura do metodo
+    // public boolean isEmpty()
+
+    // 2 - implemente o método size
+    /*
+     * Retorna o numero de elementos da lista.
+     *
+     * @return o numero de elementos da lista
+     */
+    //// assinatura do metodo
+    // public int size()
+
+    // 3 - implemente o método get
+    /*
+     * Retorna o elemento de uma determinada posicao da lista.
+     * 
+     * @param index a posição da lista
+     * 
+     * @return o elemento da posicao especificada
+     * 
+     * @throws IndexOutOfBoundsException se (index < 0 || index >= size())
+     */
+
+    /*
+     * Exemplo - veja o main
+     * Lista:
+     * 2
+     * 4
+     * 8
+     * lista.get(1)
+     * Elemento na segunda posicao da lista: 4
+     */
+
+    // assinatura do metodo
+    // public Integer get(int index)
+}
